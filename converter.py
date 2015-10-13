12cc042a9ba81a1bb2b8c2b12e4b7361832cd0f2
mieden-seer <mieden.batungbakal@seer-technologies.com>
2015-10-13 14:24:12 +0800

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,16 +1,15 @@
-inches = (1,23.5,44,23)
-y = ()
-unit = "cm"
+def converter(inches, unit):
+	if(unit == "km"):
+		y = map(lambda x: x*0.0000254, inches)
+	elif(unit == "m"):
+		y = map(lambda x: x*0.0254, inches)
+	elif(unit == "cm"):
+		y = map(lambda x: x*2.54, inches)
+	elif(unit == "feet"):
+		y = map(lambda x: x*0.0833333, inches)
+	elif(unit == "yard"):
+		y = map(lambda x: x*0.0277778, inches)
 
-if(unit == "km"):
-	y = map(lambda x: x*0.0000254, inches)
-elif(unit == "m"):
-	y = map(lambda x: x*0.0254, inches)
-elif(unit == "cm"):
-	y = map(lambda x: x*2.54, inches)
-elif(unit == "feet"):
-	y = map(lambda x: x*0.0833333, inches)
-elif(unit == "yard"):
-	y = map(lambda x: x*0.0277778, inches)
+	print y
 
-print y
+converter((1,2,3,4,5), "cm")
