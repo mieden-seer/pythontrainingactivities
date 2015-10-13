1d7ebfb8a1d871f7ba4e721344b7d215b99a52ae
mieden-seer <mieden.batungbakal@seer-technologies.com>
2015-10-13 12:09:52 +0800

    Create pyramidB.py

---
+++ pyramidB.py
@@ -0,0 +1,24 @@
+def pyramidB(x):
+	num_space = x - 1
+	num_one = 1
+	space = ""
+	one = ""
+	line = ""
+
+	for num in xrange(1, x + 1):
+		for s in xrange(num_space):
+			space += " "
+
+		for o in xrange(num_one):
+			one += "1 "
+
+		line = space + one
+		print line
+
+		space = ""
+		one = ""
+
+		num_space -= 1
+		num_one += 1
+
+pyramidB(2)
